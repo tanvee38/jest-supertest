a567f2c75585e5d7ea702dfcbbe4eb980c3b9385
mtanvee <mtanvee@athabascau.ca>
2021-02-01 22:08:40 -0700

    Revert "Add basic tests"
    This reverts commit ab602ee980ddfc0af1a5faad75e4721d8edfa686.

--- __tests__/sample.rest.tests.jest-supertest.js
+++ __tests__/sample.rest.tests.jest-supertest.js
@@ -2,20 +2,14 @@ jest.setTimeout(30000);
 
 const {JSONPath} = require('jsonpath-plus');
 
-const baseURL = 'https://localhost:45678';
-
-const request = require('supertest')(baseURL);
+const request = require('supertest')('https://reqres.in');
 
 describe(`API Tests with Supertest`, () => {
   test('Simple test', async () => {
-    const pidm = 943299;
+    const response = await request.get(`/api/users?page=2`);
 
-    const response = await request.get(`/student-registration-service/student-registration/by/student/pidm/${pidm}`).trustLocalhost();
+    const result = JSONPath({path: "data[?(@.first_name==='Michael')].last_name", json: response.body});
 
-    const result = response.body[0];
-
-    expect(response.status).toBe(200);
-
-    expect(result.key.pidm).toBe(943299);
+    expect(result[0]).toBe('Lawson');
   });
 })
